1ccdda4e198c6dc3d51cd3dc8bd8dc7465f35802
tungks <kechoplay@gmail.com>
2021-05-28 21:30:00 +0700

    fix

--- app/Http/Controllers/Admin/BridalController.php
+++ app/Http/Controllers/Admin/BridalController.php
@@ -197,14 +197,14 @@ class BridalController extends Controller
     public function deleteStyle(Request $request)
     {
         $id = $request->id;
-        $style = WeddingDressCategory::where('id', $id)->first();
-        if ($style) {
+        $dress = WeddingDressCategory::where('category_id', $id)->first();
+        if ($dress) {
             $errors = ['Có sản phẩm trong mẫu này. Hãy xóa nó trước'];
             Session::flash('errors', $errors);
             return redirect()->back();
         }
 
-        $style->delete();
+        WeddingDressCategory::find($id)->delete();
         return redirect()->route('admin.listStyle');
     }
 
